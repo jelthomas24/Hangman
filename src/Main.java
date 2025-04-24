08baf6ae1399a62940857810e3e2931b2b495129
Jerry Thomas <jerrythomas@Jerrys-MacBook-Pro.local>
2025-04-24 17:46:31 -0400

    Added additional words to words.txt

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import static javafx.application.Application.launch;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
